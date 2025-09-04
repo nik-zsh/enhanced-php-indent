90d231fdeb91f1ccc0da6661d5f5909d82ab92a9
nik-zsh <nikhillguptaa@protonmail.com>
2025-09-04 21:19:32 +0530

    Update indent.lua

--- lua/enhanced-php-indent/indent.lua
+++ lua/enhanced-php-indent/indent.lua
@@ -1,25 +1,20 @@
 local M = {}
 
 local function setup_php_buffer()
-  -- SIMPLE setup - no interfering autocmds
   vim.bo.indentexpr = "v:lua.EnhancedPhpIndent()"
-
-  -- FIXED: Simple indentkeys - no aggressive auto-indenting
   vim.bo.indentkeys = "0{,0},0),0],:,o,O,e"
-
   vim.bo.smartindent = false
   vim.bo.cindent = false
-  vim.bo.autoindent = false
   vim.b.did_indent = 1
 
-  print("Enhanced PHP Indent (Simple) loaded for: " .. vim.fn.expand('%:t'))
+  print("MINIMAL PHP Indent loaded")
 end
 
 function M.setup()
   vim.api.nvim_create_autocmd("FileType", {
     pattern = "php",
     callback = setup_php_buffer,
-    group = vim.api.nvim_create_augroup("EnhancedPHPIndent", { clear = true }),
+    group = vim.api.nvim_create_augroup("MinimalPHPIndent", { clear = true }),
   })
 end
 
