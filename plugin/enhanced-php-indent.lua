97b8bfdbd5de439526935b01013896399b066266
nik-zsh <nikhillguptaa@protonmail.com>
2025-09-04 20:40:44 +0530

    Update enhanced-php-indent.lua

--- plugin/enhanced-php-indent.lua
+++ plugin/enhanced-php-indent.lua
@@ -1,28 +1,17 @@
--- Enhanced PHP Indent Plugin Auto-loader
 if vim.g.loaded_enhanced_php_indent then
   return
 end
 vim.g.loaded_enhanced_php_indent = 1
 
--- Check Neovim version
-if vim.fn.has("nvim-0.7") ~= 1 then
-  vim.notify("enhanced-php-indent requires Neovim >= 0.7", vim.log.levels.ERROR)
-  return
-end
-
--- Commands for manual testing
-vim.api.nvim_create_user_command("EnhancedPhpIndentSetup", function()
-  require("enhanced-php-indent").setup({
-    indent_function_call_parameters = true,
-    enable_real_time_indent = true,
-  })
-  print("Enhanced PHP Indent setup complete!")
-end, {})
-
-vim.api.nvim_create_user_command("EnhancedPhpIndentStatus", function()
-  local config = require("enhanced-php-indent").config
+vim.api.nvim_create_user_command("PHPIndentTest", function()
+  local status = {
+    loaded = vim.b.did_indent == 1,
+    indentexpr = vim.bo.indentexpr,
+    global_func = _G.EnhancedPhpIndent ~= nil,
+    filetype = vim.bo.filetype
+  }
   print("Enhanced PHP Indent Status:")
-  print("  Loaded: " .. (vim.b.did_indent and "Yes" or "No"))
-  print("  IndentExpr: " .. vim.bo.indentexpr)
-  print("  Config: " .. vim.inspect(config))
+  for k, v in pairs(status) do
+    print("  " .. k .. ": " .. tostring(v))
+  end
 end, {})
